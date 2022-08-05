3420a648d400bf47e609d519f32203ebff2f9eb3
Sarmient02 <javiersarmiento02@gmail.com>
2022-08-05 01:52:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 localStorage.clear();
 
+var editando = false;
+
 function editar() {
   var gusto = document.getElementById("g").value;
   document.getElementById(
@@ -8,8 +10,6 @@ function editar() {
 }
 
 function hacerEditable(fila) {
-  var editando = false;
-
   if (editando === false) {
     var nodetr = fila.parentNode.parentNode;
     var nodestr = nodetr.getElementsByTagName("td");
@@ -22,25 +22,28 @@ function hacerEditable(fila) {
       '<td><input type="text" name="porcentaje" id="tdporcentaje" class="input" value="' +
       "0" +
       '"></td>' +
-      'En edición</td>';
-  }
-  nodetr.innerHTML = codigohtml;
+      "En edición</td>";
+    nodetr.innerHTML = codigohtml;
 
     confirm.innerHTML =
-    '<p>Pulse Aceptar para guardar los cambios o Cancelar para anularlos<p>' +
-    '<button type="submit" class="button is-success is-outlined" id="aceptar" onclick="guardar()">Aceptar</button>' +
-    '<span> </span>'+
-    '<button type="reset" class="button is-danger is-outlined" id="cancelar" onclick="location.reload()">Cancelar</button></div>';
+      "<p>Pulse Aceptar para guardar los cambios o Cancelar para anularlos<p>" +
+      '<button type="submit" class="button is-success is-outlined" id="aceptar" onclick="guardar()">Aceptar</button>' +
+      "<span> </span>" +
+      '<button type="reset" class="button is-danger is-outlined" id="cancelar" onclick="location.reload()">Cancelar</button></div>';
+  } else {
+    alert("Solo se puede editar una línea. Recargue la página para poder editar otra.");
+  }
 
   editando = true;
 }
 
-function guardar()
-{
-    localStorage.setItem('nombre', document.getElementById('nombre').value);
-    localStorage.setItem('email', document.getElementById('email').value);
-    localStorage.setItem('telefono', document.getElementById('telefono').value);
-    localStorage.setItem('gusto', document.getElementById('tdgusto').value);
-    localStorage.setItem('porcentaje', document.getElementById('tdporcentaje').value);
+function guardar() {
+  localStorage.setItem("nombre", document.getElementById("nombre").value);
+  localStorage.setItem("email", document.getElementById("email").value);
+  localStorage.setItem("telefono", document.getElementById("telefono").value);
+  localStorage.setItem("gusto", document.getElementById("tdgusto").value);
+  localStorage.setItem(
+    "porcentaje",
+    document.getElementById("tdporcentaje").value
+  );
 }
-
